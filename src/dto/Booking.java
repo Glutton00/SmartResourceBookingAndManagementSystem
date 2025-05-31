0186e4a821a74071a62defe4e4b9a8b19732f9e0
Vaishnavi <pandeyvaishnavi0805@gmail.com>
2025-05-31 09:06:13 +0530

    Adding the Booking DTO

---
+++ src/dto/Booking.java
@@ -0,0 +1,35 @@
+package dto;
+
+import java.util.Date;
+
+public class Booking {
+
+	 private String bookingId;
+	    private Date startDate;
+	    private Date endDate;
+
+	    public Booking(String bookingId, Date startDate, Date endDate) {
+	        this.bookingId = bookingId;
+	        this.startDate = startDate;
+	        this.endDate = endDate;
+	    }
+
+	    public String getBookingId() { 
+	    	return bookingId; 
+	    }
+	    public Date getStartDate() { 
+	    	return startDate; 
+	    }
+	    public Date getEndDate() { 
+	    	return endDate; 
+	    }
+
+	    public void setStartDate(Date startDate) { 
+	    	this.startDate = startDate; 
+	    }
+	    public void setEndDate(Date endDate) { 
+	    	this.endDate = endDate; 
+	    }
+	    
+}
+
