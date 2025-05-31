d5984ea6318d9a827740b0db2aca670cae1a2114
garvesh <garveshtiwari1998@gmail.com>
2025-05-31 10:03:58 +0530

    admin class added

---
+++ src/dto/Admin.java
@@ -0,0 +1,14 @@
+package dto;
+
+public class Admin extends User {
+    private String role;
+
+	public Admin(String userId, String userName, String userPassword, String email, String phoneNumber, String role) {
+	    super(userId, userName, userPassword, email, phoneNumber, "ADMIN");
+	    this.role = role;
+    }
+
+	public String getRole() { 
+    	return role; 
+    }
+}
