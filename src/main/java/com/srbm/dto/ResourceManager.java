4cd48cc3c80ccd077832dd4b2e0e82d7fba12996
Yatin Gill <gillyatin9012@gmail.com>
2025-05-31 15:56:52 +0530

    Adding admin and resoure manager

---
+++ src/main/java/com/srbm/dto/ResourceManager.java
@@ -0,0 +1,14 @@
+package com.srbm.dto;
+
+public class ResourceManager extends User {
+    private String role;
+
+    public ResourceManager(String userId, String userName, String userPassword,String email,String phoneNumber, String role){
+        super(userId, userName, userPassword,   email, phoneNumber, "RESOURCE_MANAGER");
+        this.role = role;
+    }
+
+    public String getRole() {
+         return this.role; 
+    }
+}
